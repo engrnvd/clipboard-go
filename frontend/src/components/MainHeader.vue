25facac82c1bc631aa587ad39cc4e4869c9eae71
Naveed Hassan <naveedul.hassan@innoscripta.com>
2024-11-29 20:35:46 +0500

    added shadcn

--- frontend/src/components/MainHeader.vue
+++ frontend/src/components/MainHeader.vue
@@ -3,7 +3,7 @@ import { env } from '../env'
 </script>
 
 <template>
-    <div class="px-4 py-2">
+    <div class="px-4 py-2 bg-primary text-background">
         <h1>{{ env.appName }}</h1>
     </div>
 </template>
